aa56ddccd2d5b530c6d7cf843cdce8dbeb875266
ali-hazime <a.hazime@outlook.com>
2020-12-11 06:29:59 -0500

    worked on portfolio page layout
    Started working portfolio page layout, removed some js code im no longer using.

--- js/main.js
+++ js/main.js
@@ -48,80 +48,15 @@ jQuery(document).ready(function(){
 		}
 	);
 
-	// Menu Navigation
-	 $(".menu .tabs").carouFredSel({
-        responsive          : true,
-        direction           : "left",
- 	    circular: false,
-    	infinite: false,
-        pagination  		: "#menu-controls",  
-        auto    			: false,
-        scroll 			: {
-            items           : 1,
-            duration        : 300,                        
-            wipe    : true
-        },
-		prev	: {	
-			button	: "#menu-prev",
-			key		: "right"
-		},
-		next	: { 
-			button	: "#menu-next",
-			key		: "left"
-		},
-	    swipe: {
-	        onTouch: true
-	    },
-        items: {
-            width: 140,
-            visible: {
-              min: 2,
-              max: 5
-            }
-        }           
-    });
-	/* ---------------------------------------------------------------------- */
-	/*	Cats Filter
-	/* ---------------------------------------------------------------------- */ 
-	
-	var $catsfilter 		= $('.cats-filter');
-
-	// Copy categories to item classes
-	$catsfilter.find('a').click(function() {
-		var currentOption = $(this).attr('data-filter');
-		$(this).parent().parent().find('a').removeClass('current');
-		$(this).addClass('current');
-	});	
-
 	/* ---------------------------------------------------------------------- */
 	/*	Portfolio
 	/* ---------------------------------------------------------------------- */ 
 	
 	// Needed variables
 	var $plist	 	= $('#portfolio-list');
-	var $pfilter 		= $('#portfolio-filter');
 		
 	// Run Isotope  
 	$plist.isotope({
-		filter				: '*',
 		layoutMode   		: 'masonry',
-		animationOptions	: {
-		duration			: 750,
-		easing				: 'linear'
-	   }
 	});	
-	
-	// Isotope Filter 
-	$pfilter.find('a').click(function(){
-	  var selector = $(this).attr('data-filter');
-		$plist.isotope({ 
-		filter				: selector,
-		animationOptions	: {
-		duration			: 750,
-		easing				: 'linear',
-		queue				: false,
-	   }
-	  });
-	  return false;
-	});	 
 });	
